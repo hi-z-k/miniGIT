20c859d9a077c15484eda7f0eeeded35e12d0cb1
Hizkeal <104999321+hi-z-k@users.noreply.github.com>
2025-06-20 07:24:51 +0300

    New Structure for miniGit

---
+++ minGit.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include <unordered_map>
+#include <unordered_set>
+#include <filesystem>
+#include <chrono>
+#include <openssl/sha.h>
+#include <openssl/evp.h>
+#include <queue>
+
+
+using namespace std;
+using namespace filesystem;
+
+struct CommitNode {
+    string id;
+    string stageSnap;
+    string prevCommit;
+    string timestamp;
+    string author;
+    string comment;
+};
+
+class MinGit {
+private:
+    path repoPath;
+    string repoName;
+    string author;
+
+
+
+    // void recordAuthor(const string& branch, const string& author);
+    // unordered_map<string, string> stageOf(const path& stagePath);
+    // string blob(const path& filePath);
+    // string activeBranch();
+    // string hashOf(const path& filePath);
+    // string hashOf(const string& content);
+
+
+    // string latestCommit();
+    // string latestCommit(const string& branch);
+    // string Author(const string& branch);
+    // string commitString(const CommitNode& node);
+
+
+    // string findHash(const string& targetName);
+    // void recover(const string& commitID);
+
+
+    // unordered_map<string, string> stageOf(const string& commitID);
+    // CommitNode CommitData(const string& id);
+
+
+    // string manageConflict(const string& base, const string& current, const string& source);
+    // string mergeStage(const unordered_map<string, string>& stageMap);
+    // string time();
+    // string commonAncestor(const string& commitA, const string& commitB);
+
+
+public:
+
+    MinGit(const string& path, const string& author) {
+        repoPath = path + "/.minigit";
+        repoName = repoPath.parent_path().filename().string();
+        this->author = author;
+    }
+    // void init();
+    // void add(const path& filePath);
+    // void commit(const string& comment);
+    // void log();
+    // void branch(const string& name, const string& author);
+    // void checkout(const string& targetHash);
+    // void merge(const string& branch);
+};
